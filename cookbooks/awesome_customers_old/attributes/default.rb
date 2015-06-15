5daccea0621c8c7866b9a7fe6925f29362af8e36
aaksud <deshpande.aakash@gmail.com>
2015-06-15 12:23:57 -0400

    Removing Unwanted  Data

--- cookbooks/awesome_customers_old/attributes/default.rb
+++
@@ -1,9 +0,0 @@
-default['awesome_customers']['user'] = 'web_admin'
-default['awesome_customers']['group'] = 'web_admin'
-
-default['awesome_customers']['name'] = 'customers'
-default['awesome_customers']['config'] = 'customers.conf'
-
-default['apache']['docroot_dir'] = '/srv/apache/customers'
-
-default['iptables']['install_rules'] = false
